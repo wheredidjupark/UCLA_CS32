7dbf138963181147972e918ad04a0be729c9dc24
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-10-31 23:09:21 -0700

    Finished HW2 Problem 1

---
+++ Code_Practice/StackClass Practice/StackClass Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StackClass Practice
+//
+//  Created by Ju Hyeon Park on 10/31/14.
+//  Copyright (c) 2014 Ju Hyeon Park. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
