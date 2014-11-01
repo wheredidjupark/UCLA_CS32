f8e081dc178fbc0d4d70beec1956322c7cded148
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-10-31 23:09:48 -0700

    Finished HW2 Problem1

--- CS32_Homework2/CS32_Homework2/main.cpp
+++ CS32_Homework2/CS32_Homework2/main.cpp
@@ -1,15 +1,194 @@
-//
-//  main.cpp
-//  CS32_Homework2
-//
-//  Created by Ju Hyeon Park on 10/27/14.
-//  Copyright (c) 2014 Ju Hyeon Park. All rights reserved.
-//
-
+#include <stack>
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+const char WALL = 'X';
+const char DISCOVERED_SPACE = ',';
+const char SPACE = '.';
+const int ROW_SIZE = 10;
+const int COL_SIZE = 10;
+
+//looking at the ith row's 10 columns
+bool pathExists(char maze[][COL_SIZE], int r, int c, int r2, int c2);
+bool insideMaze(char maze[][COL_SIZE], int r, int c);
+/*
+ Write a C++ function named pathExists that determines whether or not a there's a path from start to finish in a 10x10 maze. Here is the prototype:
+ 
+ 
+ bool pathExists(char maze[][10], int sr, int sc, int er, int ec);
+ // Return true if there is a path from (sr,sc) to (er,ec)
+ // through the maze; return false otherwise
+ 
+ The parameters are
+ 
+ A 2-dimensional 10x10 character array of Xs and dots that represents the maze. Each 'X' represents a wall, and each '.' represents a walkway.
+ The starting coordinates in the maze: sr, sc; the row number and column number range from 0 to 9.
+ The ending coordinates in the maze: er, ec; the row number and column number range from 0 to 9.
+ The function must determine if there is a path from maze[sr][sc] to maze[er][ec] that includes only walkways, no walls. The path may turn north, east, south, and west, but not diagonally. You must use a stack data structure.
+ 
+ 
+ (Our convention is that (0,0) is the northwest (upper left) corner, with south (down) being the increasing r direction and east (right) being the increasing c direction.)
+ 
+ The stack you must use is a stack of Coords. You may either write your own stack class, or use the stack class in the C++ Standard Library (pp. 309-311 in the textbook discuss that class). Here's an example of the relevant functions in the library's stack class:
+ 
+ 
+ #include <stack>
+ using namespace std;
+ 
+ int main()
+ {
+ stack<Coord> coordStack;     // declare a stack of Coords
+ 
+ Coord a(5,6);
+ coordStack.push(a);          // push the coordinate (5,6)
+ coordStack.push(Coord(3,4)); // push the coordinate (3,4)
+ ...
+ Coord b = coordStack.top();  // look at top item in the stack
+ coordStack.pop();            // remove the top item from stack
+ if (coordStack.empty())      // Is the stack empty?
+ cout << "empty!" << endl;
+ cout << coordStack.size() << endl;  // num of elements
+ }
+ 
+ Here is pseudocode for your function:
+ 
+ 
+ Push the starting coordinate (sr,sc) onto the coordinate stack and
+ update maze[sr][sc] to indicate that the algorithm has encountered
+ it (i.e., set maze[sr][sc] to have a value other than '.').
+ While the stack is not empty,
+ {   Pop the top coordinate off the stack. This gives you the current
+ (r,c) location that your algorithm is exploring.
+ If the current (r,c) coordinate is equal to the ending coordinate,
+ then we've solved the maze so return true!
+ Check each place you can move from the current cell:
+ If you can move NORTH and haven't encountered that cell yet, qs    w
+ then push the coordinate (r-1,c) onto the stack and update
+ maze[r-1][c] to indicate the algorithm has encountered it.
+ If you can move EAST and haven't encountered that cell yet,
+ then push the coordinate (r,c+1) onto the stack and update
+ maze[r][c+1] to indicate the algorithm has encountered it.
+ If you can move SOUTH and haven't encountered that cell yet,
+ then push the coordinate (r+1,c) onto the stack and update
+ maze[r+1][c] to indicate the algorithm has encountered it.
+ If you can move WEST and haven't encountered that cell yet,
+ then push the coordinate (r,c-1) onto the stack and update
+ maze[r][c-1] to indicate the algorithm has encountered it.
+ }
+ There was no solution, so return false
+ 
+ Because the focus of this homework is on practice with the data structures, we won't demand that your function be as robust as we normally would. In particular, your function may make the following simplifying assumptions (i.e., you do not have to check for violations):
+ 
+ the maze has 10 rows;
+ the maze contains only Xs and dots when passed in to the function;
+ the top and bottom rows of the maze contain only Xs, as do the left and right columns;
+ sr, sc, er, and ec are between 0 and 9;
+ maze[sr][sc] and maze[er][ec] are '.' (i.e., not walls)
+ (Of course, since your function is not checking for violations of these conditions, make sure you don't pass bad values to the function when you test it.)
+ 
+ For this part of the homework, you will turn in one file named mazestack.cpp that contains the Coord class and your stack-based pathExists function. (Do not leave out the Coord class and do not put it in a separate file.) If you use the library's stack type, your file should include the appropriate header.
+ */
+class Coord
+{
+public:
+    Coord(int rr, int cc) : m_r(rr), m_c(cc){} ;
+    int r() const
+    {
+        return m_r;
+    }
+    
+    int c() const
+    {
+        return m_c;
+    }
+private:
+    int m_r;
+    int m_c;
+};
+
+
+int main()
+{
+    
+    
+    char maze[ROW_SIZE][COL_SIZE] = {
+        { 'X','X','X','X','X','X','X','X','X','X'},
+        { 'X','.','.','.','.','.','.','.','.','X'},
+        { 'X','X','.','X','.','X','X','X','X','X'},
+        { 'X','.','.','X','.','X','.','.','.','X'},
+        { 'X','.','.','X','.','.','.','X','.','X'},
+        { 'X','X','X','X','.','X','X','X','.','X'},
+        { 'X','.','.','X','.','.','.','X','X','X'},
+        { 'X','.','.','X','X','.','X','X','.','X'},
+        { 'X','.','.','.','X','.','.','.','.','X'},
+        { 'X','X','X','X','X','X','X','X','X','X'}
+    };
+    
+    
+    
+    if(pathExists(maze, 6, 4, 1, 1))
+    {
+        cout <<"Solvable!\n";
+    }
+    else
+        cout <<"Cannot Solve\n";
+    
     return 0;
+}
+
+bool pathExists(char maze[][COL_SIZE], int sr, int sc, int er, int ec)
+{
+    stack<Coord> coordStack;
+    Coord a(sr,sc);
+    maze[sr][sc] = DISCOVERED_SPACE;
+    Coord b(er,ec);
+    coordStack.push(a);
+   
+    while (coordStack.empty() == false)
+    {
+        a = coordStack.top(); //uses the default operator 
+        coordStack.pop();
+        
+        if(a.r() == b.r() && a.c() == b.c())
+        {
+            return true;
+        }
+        
+        if(insideMaze(maze, a.r()-1 , a.c())) //check north
+        {
+            coordStack.push(Coord(a.r()-1, a.c()));
+            maze[a.r()-1][a.c()] = DISCOVERED_SPACE;
+        }
+        if(insideMaze(maze, a.r(), a.c()+1))
+        {
+            coordStack.push(Coord(a.r(), a.c()+1));
+            maze[a.r()][a.c()+1] = DISCOVERED_SPACE;
+        }
+        if(insideMaze(maze, a.r()+1, a.c()))
+        {
+            coordStack.push(Coord(a.r()+1, a.c()));
+            maze[a.r()+1][a.c()] = DISCOVERED_SPACE;
+        }
+        if(insideMaze(maze, a.r(), a.c()-1))
+        {
+            coordStack.push(Coord(a.r(), a.c()-1));
+            maze[a.r()][a.c()-1] = DISCOVERED_SPACE;
+        }
+        
+    }
+    return false;
+}
+
+bool insideMaze(char maze[][COL_SIZE], int r, int c)
+{
+    if(!(r >= 0 && r < ROW_SIZE && c>= 0 && c < COL_SIZE))
+    {
+        return false;
+    }
+    else
+    {
+        if (maze[r][c] != WALL && maze[r][c] != DISCOVERED_SPACE)
+            return true;
+    }
+    return false;
 }
--- Code_Practice/StackClass Practice/StackClass Practice/main.cpp
+++ Code_Practice/StackClass Practice/StackClass Practice/main.cpp
@@ -8,8 +8,67 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+typedef int ItemType;
+const size_t STACK_MAXSIZE = 100;
+
+class Stack
+{
+public:
+    Stack();
+    bool push(const ItemType& item);
+    ItemType top();
+    void pop();
+    
+private:
+    ItemType stack[STACK_MAXSIZE];
+    int m_size;
+    
+};
+
+Stack::Stack()
+{
+    m_size=0;
+}
+bool Stack::push(const ItemType& item)
+{
+    if(m_size >= STACK_MAXSIZE)
+        return false;
+    
+    stack[m_size] = item;
+    m_size++;
+    return true;
+}
+
+ItemType Stack::top()
+{
+    if(m_size == 0)
+    {
+        return NULL; //give no element
+    }
+    return stack[m_size-1];
+}
+
+void Stack::pop()
+{
+    if(m_size ==0)
+    {
+        std::cerr<<"no item in the Stack!";
+        return;
+    }
+    
+    stack[m_size-1] = NULL;
+    m_size--;
+}
+
+
+
+int main(int argc, const char * argv[])
+{
+    Stack istack;
+    istack.push(1);
+    std::cout <<istack.top() <<std::endl;
+    istack.pop();
+    istack.pop();
+    
 }
